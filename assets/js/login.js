3fac8c45f8cec752169ef7672f23c179c97875ff
wangyushan <320761026@qq.com>
2020-12-13 10:02:20 +0800

    登录注册细节完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,59 +1,69 @@
 $(function () {
   let layer = layui.layer;
-  $("#loginbtn").click(function () {
-    $(".regist").show();
-    $(".login").hide();
-  });
+  let form = layui.form;
   $("#registbtn").click(function () {
     $(".regist").hide();
     $(".login").show();
   });
-  let form = layui.form;
+  $("#loginbtn").click(function () {
+    $(".login").hide();
+    $(".regist").show();
+  });
   form.verify({
+    repass: function (value) {
+      console.log(value);
+      if ($(".repass").val() !== value) {
+        return "两次输入的密码不一致";
+      }
+    },
     //我们既支持上述函数式的方式，也支持下述数组的形式
     //数组的两个值分别代表：[正则匹配、匹配不符时的提示文字]
     pass: [/^[\S]{6,12}$/, "密码必须6到12位，且不能出现空格"],
-    repass: function (value) {
-      //   console.log(value);
-      //   console.log($(".repass").val());
-      if ($(".repass").val() !== value) {
-        return "两次密码输入不一致";
-      }
-    },
   });
-  //   注册发送ajax
+  // 配置url地址
+  $.ajaxPrefilter(function (options) {
+    options.url = "http://ajax.frontend.itheima.net" + options.url;
+  });
+  // 注册表单
   $(".registform").submit(function (e) {
     e.preventDefault();
     let data = $(this).serialize();
     $.ajax({
       type: "POST",
-      url: "http://ajax.frontend.itheima.net/api/reguser",
+      url: "/api/reguser",
       data: data,
       success: function (res) {
-        // console.log(res);
+        console.log(res);
         if (res.status !== 0) {
           return layer.msg(res.message);
         }
-        layer.msg(res.message);
-        $(".registform")[0].reset();
-        $("#registbtn").click();
+        layer.msg(
+          "注册成功,即将跳往登录页面",
+          {
+            time: 2000, //2秒关闭（如果不配置，默认是3秒）
+          },
+          function () {
+            //do something
+            $("#registbtn").click();
+          }
+        );
       },
     });
   });
-  //  发送登录ajax
   $(".loginform").submit(function (e) {
     e.preventDefault();
     let data = $(this).serialize();
     $.ajax({
       type: "POST",
-      url: "http://ajax.frontend.itheima.net/api/login",
-      data: data,
+      url: "/api/login",
+      data,
       success: function (res) {
-        if (res.status === 200) {
+        console.log(res);
+        if (res.status !== 0) {
           return layer.msg(res.message);
         }
         layer.msg(
-          "登陆成功即将跳转页面",
+          "登录成功即将跳转页面",
           {
             time: 2000, //2秒关闭（如果不配置，默认是3秒）
           },
