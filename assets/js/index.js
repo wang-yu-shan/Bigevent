e0a87753fcef94e01b1ebb0f40d565cfca8d071c
wangyushan <320761026@qq.com>
2020-12-14 18:25:31 +0800

    图表完成

---
+++ assets/js/index.js
@@ -0,0 +1,36 @@
+getuser();
+function getuser() {
+  $.ajax({
+    url: "/my/userinfo",
+    headers: {
+      // 设置headers请求头信息
+      Authorization: localStorage.getItem("data"),
+    },
+    //
+    success: function (res) {
+      // 当nickname存在时优先展示，不存在时展示username
+      let name = res.data.nickname || res.data.username;
+      $("#welcome").html("欢迎&nbsp;&nbsp;" + name);
+      if (res.data.user_pic) {
+        //  layui-nav-img 为图片头像   textpic为文字头像
+        //当res.data.user_pic存在时优先展示图片文件
+        $(".layui-nav-img").attr("src", res.data.user_pic).show();
+        $(".textpic").hide();
+      } else {
+        let ming = name[0].toUpperCase(); //获取name的首字母设置大写
+        $(".layui-nav-img").hide();
+        $(".textpic").show().text(ming);
+      }
+    },
+  });
+}
+
+$(function () {
+  let layer = layui.layer;
+  $(".quit").click(function (e) {
+    e.preventDefault();
+    layer.confirm("确认退出吗", { icon: 3 }, function (index) {
+      location.href = "/home/login.html";
+    });
+  });
+});
